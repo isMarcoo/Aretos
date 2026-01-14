4b08cfa400f902ee1b872b78570fe8e1d3c530e0
Shangda <763526010@qq.com>
2026-01-14 20:17:09 +0800

    dev: implement GLM api and passed tests

--- tests/test_llm_integration.py
+++ tests/test_llm_integration.py
@@ -2,21 +2,21 @@
 import pytest
 import os
 from models.deepseek import DeepSeekLLM
+from models.glm import GLMLLM
 from core.schemas import Message
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-@pytest.mark.skipif(not os.getenv("DEEPSEEK_API_KEY"), reason="未设置 API Key")
-def test_deepseek_real_chat():
-    api_key = os.getenv("DEEPSEEK_API_KEY")
-    llm = DeepSeekLLM(api_key=api_key)
+@pytest.mark.skipif(not os.getenv("GLM_API_KEY"), reason="未设置 API Key")
+def test_llm_real_chat():
+    api_key = os.getenv("GLM_API_KEY")
+    llm = GLMLLM(api_key=api_key)
 
     messages = [Message(role="user", content="回复‘收到’")]
     response = llm.chat(messages)
     print(response)
-    print("-------------------------------")
 
     assert response.content is not None
     assert "收到" in response.content
@@ -24,9 +24,9 @@ def test_deepseek_real_chat():
 
 
 @pytest.mark.asyncio
-async def test_deepseek_real_achat():
-    api_key = os.getenv("DEEPSEEK_API_KEY")
-    llm = DeepSeekLLM(api_key=api_key)
+async def test_llm_real_achat():
+    api_key = os.getenv("GLM_API_KEY")
+    llm = GLMLLM(api_key=api_key)
 
     messages = [Message(role="user", content="你好")]
     # 测试异步接口
